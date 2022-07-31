69e825fe1376fa34a70248e1f99809c3802112f5
Ronnie Isurina <17154820+risurina@users.noreply.github.com>
2022-07-31 23:08:59 +0800

    Create routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Location\AdministrativeAreaController;
+use App\Http\Controllers\Location\CityController;
+use App\Http\Controllers\Location\CountryController;
+use App\Http\Controllers\Location\ForecastController;
+use App\Http\Controllers\Location\RegionController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -30,6 +35,28 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
+        return Inertia::render('Dashboard/Index');
     })->name('dashboard');
 });
+
+
+
+/**
+ * Create api base data list
+ * In this way, I can reuse routes for api
+ */
+Route::middleware(['auth:sanctum', config('jetstream.auth_session')])
+    ->prefix('api')
+    ->name('api.')
+    ->group(function () {
+        Route::prefix('/location')
+            ->name('location.')
+            ->group(function () {
+                Route::get('/regions', RegionController::class)->name('regions-list');
+                Route::get('/countries', CountryController::class)->name('countries-list');
+                Route::get('/admin-area', AdministrativeAreaController::class)->name('admin-area-list');
+                Route::get('/cities', CityController::class)->name('cities-list');
+            });
+
+        Route::get('/forecast/{locationKey}', ForecastController::class)->name('forecast-get');
+    });
